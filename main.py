d6bf007b70a066d9783f7a6595aaf4dc46d310aa
Leon Dore <leon.dore@gmail.com>
2023-11-14 06:42:34 -0400

    Task 12. Solve maze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from graphics import Window
 from maze import Maze
+import sys
 
 
 def main():
@@ -8,9 +9,17 @@ def main():
     margin = 50
     screen_x = 940
     screen_y = 820
+
+    sys.setrecursionlimit(10000)
     win = Window(screen_x, screen_y)
 
-    maze = Maze(margin, margin, num_rows, num_cols, win)
+    maze = Maze(margin, margin, num_rows, num_cols, win, 10)
+    print("maze created")
+    is_solveable = maze.solve()
+    if not is_solveable:
+        print("maze can not be solved!")
+    else:
+        print("maze solved!")
 
     win.wait_for_close()
 
